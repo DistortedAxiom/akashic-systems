a4dbb189c79acbb02aab1d05679b171e93dcb5a0
DistortedAxiom <48456332+DistortedAxiom@users.noreply.github.com>
2019-06-10 19:53:33 -0400

    changed hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-import backgroundvid from './nebula.mp4'
+import backgroundvid from './black.mp4';
+import './Hero.css';
 
 export default class BgVideo extends Component {
     render() {
         return (
-            <video className="vid" autoPlay loop muted>
+            <video class="video-container" autoPlay loop muted>
                 <source src={backgroundvid} type='video/mp4' />
             </video>
         )
