e3abba2d53a3bf7c1a425ed47c4988b06e66c753
DistortedAxiom <48456332+DistortedAxiom@users.noreply.github.com>
2019-06-24 22:03:31 -0400

    background color for mainapge

--- src/pages/mainpage/MainPage.jsx
+++ src/pages/mainpage/MainPage.jsx
@@ -1,24 +1,12 @@
 import React from 'react';
 import Plot from 'react-plotly.js';
+import './MainPage.css';
 
 export default class MainPage extends React.Component {
+
     render() {
     return (
-        <div>
-        <h1>Test</h1>
-        <Plot
-        data={[
-          {
-            x: [1, 2, 3],
-            y: [2, 6, 3],
-            type: 'scatter',
-            mode: 'lines+markers',
-            marker: {color: 'red'},
-          },
-          {type: 'bar', x: [1, 2, 3], y: [2, 5, 3]},
-        ]}
-        layout={{width: 320, height: 240, title: 'A Fancy Plot'}}
-      />
+      <div className="container-fluid">
       </div>
     );
     }
