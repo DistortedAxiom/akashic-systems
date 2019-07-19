77f6aa6a0a729d89336d7eb59736d80300d38fb9
DistortedAxiom <48456332+DistortedAxiom@users.noreply.github.com>
2019-07-19 16:40:08 -0400

    added plotly with nav

--- src/pages/mainpage/MainPage.jsx
+++ src/pages/mainpage/MainPage.jsx
@@ -3,6 +3,7 @@ import Plot from 'react-plotly.js';
 import SideBar from './main_components/sidebar';
 import './MainPage.css';
 import SideNav, { Toggle, Nav, NavItem, NavIcon, NavText } from '@trendmicro/react-sidenav';
+import Plotly from './main_components/plotly';
 
 // Be sure to include styles at some point, probably during your bootstraping
 import '@trendmicro/react-sidenav/dist/react-sidenav.css';
@@ -21,7 +22,7 @@ export default class MainPage extends React.Component {
     <SideNav.Nav defaultSelected="home">
         <NavItem eventKey="home">
             <NavIcon>
-                <i className="fa fa-fw fa-home" style={{ fontSize: '1.75em' }} />
+                <i className="fa fa-fw fa-home" style={{ fontSize: '1.75em',}} />
             </NavIcon>
             <NavText>
                 Home
@@ -47,7 +48,10 @@ export default class MainPage extends React.Component {
         </NavItem>
     </SideNav.Nav>
 </SideNav>
+      <div className="plot-div">
+      <Plotly />
       </div>
+    </div>
     );
     }
 }
