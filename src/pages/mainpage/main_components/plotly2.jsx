93297a0faae2d91d344f79b4bb51e99d9f136c9d
DistortedAxiom <48456332+DistortedAxiom@users.noreply.github.com>
2019-07-28 16:06:48 -0400

    state manipulation for csv data

--- src/pages/mainpage/main_components/plotly2.jsx
+++ src/pages/mainpage/main_components/plotly2.jsx
@@ -1,26 +1,42 @@
 import React from 'react';
 import Plot from 'react-plotly.js';
+import { PassThrough } from 'stream';
 
 export default class Plotly2 extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.updateData = this.updateData.bind(this);
+    this.state = {
+      data: null,
+    }
+  }
 
   componentDidMount() {
     const csv_url = "https://raw.githubusercontent.com/plotly/datasets/master/finance-charts-apple.csv";
     var Papa = require('papaparse')
 
-    let results
 
     Papa.parse(csv_url, {
       dynamicTyping: true,
       download: true,
-      header: true,
       comments: "*=",
-      complete: function(data) {
-        console.log(data)
-      }
-});
+      header: true,
+      complete: this.updateData,
+    });
   }
 
+  updateData(result) {
+    const data =  result.data
+    this.setState({data: data})
+  }
+
+
   render() {
+
+    const filler = this.state.data
+    console.log(filler);
 
     var trace1 = {
       x: ['giraffes', 'orangutans', 'monkeys'],
@@ -40,14 +56,13 @@ export default class Plotly2 extends React.Component {
     
     var layout = {barmode: 'group'};
 
-
     return (
+
       <Plot
         data = {data}
         layout = {layout}
       />
 
-      
     );
   }
 }
