0a10f17a70246b8b5499af9318a712a4326fff44
DistortedAxiom <48456332+DistortedAxiom@users.noreply.github.com>
2019-07-29 15:43:50 -0400

    turned object references into string for plot usage

--- src/pages/mainpage/main_components/plotly2.jsx
+++ src/pages/mainpage/main_components/plotly2.jsx
@@ -9,7 +9,9 @@ export default class Plotly2 extends React.Component {
 
     this.updateData = this.updateData.bind(this);
     this.state = {
-      data: null,
+      data: {},
+      x_array : [],
+      y_array : []
     }
   }
 
@@ -21,22 +23,32 @@ export default class Plotly2 extends React.Component {
     Papa.parse(csv_url, {
       dynamicTyping: true,
       download: true,
-      comments: "*=",
       header: true,
+      comments: "*=",
       complete: this.updateData,
     });
   }
 
   updateData(result) {
-    const data =  result.data
-    this.setState({data: data})
+    const data =  result.data;
+    var x_array = [];
+    var y_array = [];
+    for (var i = 0; i < data.length; i++) {
+      x_array.push(data[i]["Date"]);
+      y_array.push(data[i]["AAPL.High"]);
+    }
+    this.setState({data: data});
+    this.setState({x_array: x_array});
+    this.setState({y_array: y_array});
   }
 
 
   render() {
 
-    const filler = this.state.data
-    console.log(filler);
+    let main_data = this.state.data;
+    console.log(this.state.x_array);
+    console.log(this.state.y_array);
+
 
     var trace1 = {
       x: ['giraffes', 'orangutans', 'monkeys'],
@@ -57,7 +69,6 @@ export default class Plotly2 extends React.Component {
     var layout = {barmode: 'group'};
 
     return (
-
       <Plot
         data = {data}
         layout = {layout}
